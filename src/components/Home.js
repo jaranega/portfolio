a6e20fa5c19209b805af28bbb7127c3fdaf3c7c0
Javi Aránega <javi.aranega@gmail.com>
2021-03-23 19:59:00 +0100

    Basic Navigation

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Home() {
+    return (
+        <main>
+            <img></img>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64">
+                <h1 className="text-4xl text-red-500 font-bold leading-none lg:leading-snug">Hi! I'm a Creative Coder!</h1>
+            </section>
+        </main>
+    )
+}
